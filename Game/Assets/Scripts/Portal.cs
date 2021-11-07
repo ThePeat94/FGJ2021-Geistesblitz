2cf5e65d447301649ba6e8011a7dc6f14b357ef7
hendrik.rakemann <hendrik.rakemann@innogames.com>
2021-11-07 17:42:48 +0100

    added level completed sound

--- Game/Assets/Scripts/Portal.cs
+++ Game/Assets/Scripts/Portal.cs
@@ -6,10 +6,14 @@ namespace UnityTemplateProjects
     public class Portal : MonoBehaviour
     {
         [SerializeField] private GameLevelController m_gameLevelController;
+        [SerializeField] private AudioSource m_finishedSource;
         private void OnTriggerEnter(Collider other)
         {
-            if(other.GetComponent<PlayerMovementController>() != null) 
+            if(other.GetComponent<PlayerMovementController>() != null)
+            {
                 this.m_gameLevelController.FinishLevel();
+                this.m_finishedSource.Play();
+            }
         }
     }
 }
